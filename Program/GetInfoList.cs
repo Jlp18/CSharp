8f3a588db88f1a193dc6e75b4a69e5ebf944f79c
eJlp18 <517602852@qq.com>
2018-05-05 14:25:09 +0800

    实体类

---
+++ Program/GetInfoList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Login
+{
+    class GetInfoList
+    {
+        public static ArrayList subject_id = new ArrayList();  //存储课程编号
+        public static ArrayList subject_name = new ArrayList();  //存储课程名称
+        public static ArrayList student_id = new ArrayList();  //存储学号
+        public static ArrayList student_name = new ArrayList();  //存储学生姓名
+        public static ArrayList student_class = new ArrayList();  //存储学生班级
+        public static ArrayList student_score = new ArrayList();  //存储成绩
+
+
+    }
+}
