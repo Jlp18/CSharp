9ae6b1d1195b85434ffaad37711ad381c4e75b94
eJlp18 <517602852@qq.com>
2018-05-05 14:27:08 +0800

    insert score data

---
+++ Program/grade_management.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Login
+{
+    partial class grade_management
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.学号 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.student_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.student_class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.score = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.comboBox1.Items.AddRange(new object[] {
+            "英语视听说",
+            "英语协作",
+            "书法鉴赏",
+            "市场营销导论",
+            "知识产权保护",
+            "计算机导论",
+            "数据结构"});
+            this.comboBox1.Location = new System.Drawing.Point(97, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(113, 20);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.label1.Font = new System.Drawing.Font("新宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(2);
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请选择课程";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.学号,
+            this.student_name,
+            this.student_class,
+            this.score});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(510, 291);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 27);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(324, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 26);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "返回";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // 学号
+            // 
+            this.学号.HeaderText = "学号";
+            this.学号.Name = "学号";
+            this.学号.Width = 110;
+            // 
+            // student_name
+            // 
+            this.student_name.HeaderText = "姓名";
+            this.student_name.Name = "student_name";
+            this.student_name.Width = 110;
+            // 
+            // student_class
+            // 
+            this.student_class.HeaderText = "专业班级";
+            this.student_class.Name = "student_class";
+            this.student_class.Width = 135;
+            // 
+            // score
+            // 
+            this.score.HeaderText = "成绩";
+            this.score.Name = "score";
+            // 
+            // grade_management
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(534, 362);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "grade_management";
+            this.Text = "成绩录入";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 学号;
+        private System.Windows.Forms.DataGridViewTextBoxColumn student_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn student_class;
+        private System.Windows.Forms.DataGridViewTextBoxColumn score;
+    }
+}
---
+++ Program/grade_management.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+using MySql.Data.MySqlClient;
+
+namespace Login
+{
+    public partial class grade_management : Form
+    {
+        private string id;  //课程编号
+        public grade_management()
+        {
+            InitializeComponent();
+        }
+
+        //序号自动递增
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+                e.RowBounds.Location.Y,
+                dataGridView1.RowHeadersWidth - 4,
+                e.RowBounds.Height);
+                TextRenderer.DrawText(e.Graphics, (e.RowIndex + 1).ToString(),
+                dataGridView1.RowHeadersDefaultCellStyle.Font,
+                rectangle,
+                dataGridView1.RowHeadersDefaultCellStyle.ForeColor,
+                TextFormatFlags.VerticalCenter | TextFormatFlags.Right);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string constr = "DataBase = test; Data Source = 127.0.0.1;Port = 3307;User Id=root;Password=jlp1996;pooling=false;CharSet=utf8;";
+            MySqlConnection mycon = new MySqlConnection();
+            mycon.ConnectionString = constr;
+            mycon.Open();
+
+            //查询数据
+            string get_subject_id = "select subject_id from subject where subject_name = '"+ comboBox1.Text +"' ";
+            MySqlCommand mycom = new MySqlCommand();
+            mycom.CommandText = get_subject_id;
+            MySqlDataAdapter myDA = new MySqlDataAdapter();
+            myDA.SelectCommand = mycom;
+            myDA.SelectCommand.Connection = mycon;
+            DataSet myDS = new DataSet();
+            int a = myDA.Fill(myDS);
+            if (a > 0)  //查询到结果
+            {
+                id = myDS.Tables[0].Rows[0][0].ToString();
+            }
+            else
+            {
+                MessageBox.Show("没有该门课程\n请重新选择");
+                this.comboBox1.Text = "";  //清除内容
+                comboBox1.Focus();  //聚焦comboBox1
+            }
+            
+            //录入数据
+            for(int i = 0 ; i < dataGridView1.RowCount-1 ; i++)
+            {
+                string s_score = "insert into score (subject_id,subject_name,student_id,student_name,student_class,score) values ('"+id+"','"+comboBox1.Text.Trim().ToString()+"','"+dataGridView1.Rows[i].Cells[0].Value+"','"+dataGridView1.Rows[i].Cells[1].Value+"','"+dataGridView1.Rows[i].Cells[2].Value+"','"+dataGridView1.Rows[i].Cells[3].Value+"')";
+                mycom.CommandText = s_score;
+                mycom.ExecuteNonQuery();   //执行语句
+            }
+            mycon.Close();   //关闭连接
+            mycon.Dispose();  //??释放对象
+            int count = this.dataGridView1.RowCount - 1;
+
+            MessageBox.Show("录入成功\n已录入成绩"+ count +"人");
+            dataGridView1.Rows.Clear(); //初始化dataGridView控件
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
