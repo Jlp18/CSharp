ed30e8964e21dcafc1dc459319299021cb85e239
eJlp18 <517602852@qq.com>
2018-05-05 14:18:44 +0800

    login

---
+++ Program/Login.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(120, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "用户名:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(120, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "密码:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(185, 77);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(155, 21);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(185, 144);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(155, 21);
+            this.txtPwd.TabIndex = 3;
+            this.txtPwd.UseSystemPasswordChar = true;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(118, 227);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(80, 23);
+            this.loginBtn.TabIndex = 4;
+            this.loginBtn.Text = "登录";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(260, 227);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(80, 23);
+            this.cancelBtn.TabIndex = 5;
+            this.cancelBtn.Text = "退出";
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.registerBtn.Location = new System.Drawing.Point(345, 77);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(80, 23);
+            this.registerBtn.TabIndex = 6;
+            this.registerBtn.Text = "注册?";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(480, 355);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.registerBtn);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button registerBtn;
+       
+       
+
+    }
+}
+
---
+++ Program/Login.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Login
+{
+    public partial class Login : Form
+    {
+        private int errorTime = 3;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            errorTime = errorTime - 1;
+
+            string username = txtName.Text.Trim();  //获取用户名
+            string password = txtPwd.Text.Trim();  //获取密码
+            string constr = "Database=test;Data Source=127.0.0.1;Port=3307;User Id=root;Password=jlp1996;pooling=false;CharSet=utf8;";  //设置连接字符串
+            MySqlConnection mycon = new MySqlConnection();  //实例化连接对象
+            mycon.ConnectionString = constr;
+            mycon.Open();
+
+            MySqlCommand mycom = new MySqlCommand();  //创建SQL命令执行对象
+            string s1 = "select username , password from tb1 where username = '" + username +"' and password = '"+ password +"'";
+            mycom.CommandText = s1;  //执行SQL命令
+            MySqlDataAdapter myDA = new MySqlDataAdapter();  //实例化数据适配器
+            
+            myDA.SelectCommand = mycom;  //让适配器执行SELECT命令
+            myDA.SelectCommand.Connection = mycon; //初始化
+            DataSet myDS = new DataSet();  //实例化结果数据集
+            int n = myDA.Fill(myDS , "tb1");  //将结果放入数据适配器 ， 返回元素个数
+            if (n != 0)
+            {
+                MessageBox.Show("欢迎使用!");  //登录成功
+
+                
+                Main main = new Main();
+                main.Show();
+                this.Visible = false;
+            }
+            else
+            {
+                if (errorTime > 0)
+                {
+                    MessageBox.Show("用户名或密码错误, 请重新输入");
+                    txtName.Text = ""; //清空输入框
+                    txtPwd.Text = "";
+                    txtName.Focus();  //输入光标的位置
+                }
+                else
+                {
+                    MessageBox.Show("输入错误已达三次，系统将自动退出");
+                    this.Close();
+                }
+            }
+            mycon.Close();
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void registerBtn_Click(object sender, EventArgs e)
+        {
+            
+            Register register = new Register();
+            register.Show();
+            this.Visible = false;
+        }
+
+    }
+}
